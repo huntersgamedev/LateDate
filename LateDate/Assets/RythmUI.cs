be23c4110e72eeba7d672996ed9efccad083fce7
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2018-10-14 15:29:28 -0400

    startedUI

---
+++ LateDate/Assets/RythmUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RythmUI : MonoBehaviour {
+    //angryMeterControlls
+    public Image AngryIndicator;
+    private float FullMeterX= 259f;
+    private float EmptyMeterX= 788.3f;
+    private float CurrentPosition;
+    private float MoveAmount=5.293f;  
+       
+
+	// Use this for initialization
+	void Start () {
+       // AngryIndicator.transform.position = new Vector3(CurrentPosition,AngryIndicator.transform.position.y,AngryIndicator.transform.position.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            angryMeterControl(-5);
+        }
+	}
+
+    private void angryMeterControl(float amountChanged)
+    {
+        
+        
+        if(AngryIndicator.transform.position.x <FullMeterX&&
+        AngryIndicator.transform.position.x > EmptyMeterX)
+        {
+            
+            MoveAmount = amountChanged * 5.293f;
+
+            AngryIndicator.transform.position = new Vector3(AngryIndicator.transform.position.x + MoveAmount, AngryIndicator.transform.position.y, AngryIndicator.transform.position.z);
+        }
+     
+    }
+
+
+
+}
